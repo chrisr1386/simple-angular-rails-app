9967a3d5aeacf6a34728db57e27af82ebcf40164
Chris Root <chrisr1386@gmail.com>
2013-09-11 21:19:21 -0400

    removing previous app resources and migration

--- ngapp/app/scripts/app.js
+++ ngapp/app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('notesApp', ['ngResource'])
+angular.module('myApp', ['ngResource'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- ngapp/app/scripts/controllers/main.js
+++ ngapp/app/scripts/controllers/main.js
@@ -1,25 +1,8 @@
 'use strict';
 
-angular.module('notesApp')
-.factory('Note', function ($resource) {
-  return $resource('/api/notes/:noteId');
-});
+angular.module('myApp')
+.controller('MainCtrl', function ($scope) {
 
-angular.module('notesApp')
-.controller('MainCtrl', function ($scope, Note) {
-
-  $scope.notes = Note.query();
-
-  $scope.create = function(title, body) {
-    Note.save({title: title, body: body}, function(note) {
-      $scope.notes.push(note);
-    });
-  };
-
-  $scope.delete = function(index) {
-    Note.delete({noteId: $scope.notes[index].id}, function() {
-      $scope.notes.splice(index, 1);
-    });
-  };
+  $scope.test = 'Testing'
 
 });
